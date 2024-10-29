02076279a85872119910ec5462c3fda8050967e0
divyaabaghel1509 <157110509+divyaabaghel1509@users.noreply.github.com>
2024-10-29 15:09:19 +0530

    Create Adhoc(1).sql

---
+++ Adhoc(1).sql
@@ -0,0 +1,5 @@
+
+select distinct p.product_name, base_price, f.promo_type from fact_events f 
+join dim_products as p on f.product_code = p.product_code where base_price > 500 and promo_type = "BOGOF" 
+
+
