ee5a561d1a1cb43bcb16ef34fbad8f9fa5341d73
divyaabaghel1509 <157110509+divyaabaghel1509@users.noreply.github.com>
2024-10-29 17:40:55 +0530

    Create Adhoc.(5).sql

---
+++ Adhoc.(5).sql
@@ -0,0 +1,19 @@
+    SELECT 
+    product_name,
+    category,
+    ROUND((SUM(CASE
+            WHEN promo_type = 'BOGOF' THEN base_price * 0.5 * (quantity_sold(after_promo)* 2)
+            WHEN promo_type = '500 Cashback' THEN (base_price - 500) * quantity_sold(after_promo)
+            WHEN promo_type = '50% OFF' THEN base_price * 0.5 * quantity_sold(after_promo)
+            WHEN promo_type = '33% OFF' THEN base_price * 0.67 * quantity_sold(after_promo)
+            WHEN promo_type = '25% OFF' THEN base_price * 0.75 * quantity_sold(after_promo)
+            ELSE 0
+           END) 
+           - SUM(base_price * quantity_sold(before_promo))) / SUM(base_price * quantity_sold(before_promo)) * 100,2) AS `IR%`
+      FROM
+      fact_events
+      JOIN
+      dim_products USING (product_code)
+      GROUP BY product_name , category
+      ORDER BY `IR%` DESC
+      LIMIT 5;
